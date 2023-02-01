e4380e1b5499c319855bc8472861fe3c0221768e
shokishimo <shokishimo@gmail.com>
2023-02-01 16:11:56 +0000

    Deploy on EC2 Success

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	srvMux := server.ServeMux()
 	fmt.Println("Server is running")
-	if err := http.ListenAndServe(":3000", srvMux); err != nil {
+	if err := http.ListenAndServe(":8080", srvMux); err != nil {
 		log.Fatal("Error in running the server")
 		fmt.Printf("e: %v\n", err)
 	}
